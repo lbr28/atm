6190821f6cdd93177ec665707046778a5476610d
Lillian Regula <lbregula@yahoo.com>
2016-12-01 16:38:30 -0800

    atm homework resubmit

--- src/Customer.java
+++ src/Customer.java
@@ -19,14 +19,14 @@ public class Customer {
     }
 
     public void chooseFunction() throws Exception {
-    System.out.println("What would you like to do; check balance, withdrawl, cancel");
+    System.out.println("What would you like to do; check balance, withdraw, cancel");
     String function = input.nextLine();
 
     if (function.equalsIgnoreCase("check balance")){
         System.out.println("Your balance is $100");
     }
-    else if (function.equalsIgnoreCase("withdrawl")){
-        System.out.println("How much would you like to withdrawl?");
+    else if (function.equalsIgnoreCase("withdraw")){
+        System.out.println("How much would you like to withdraw?");
         int amount = Integer.parseInt(this.input.nextLine());
         if (amount > this.balance){
             throw new Exception(("Funds not available"));
